852c3e22ef4be0599ea6ef55e6fce1befffbf65a
abdoumariox-s <abdoumariox@gmail.com>
2026-01-09 10:38:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10438166
+    "zoneId": 10438284
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
